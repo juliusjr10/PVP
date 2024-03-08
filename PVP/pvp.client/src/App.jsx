7fb39b1f0ec38df402415e8ad27cacdcbbf70ccf
tomakas54 <112956264+tomakas54@users.noreply.github.com>
2024-03-08 14:54:27 +0200

    Tailwind install

--- PVP/pvp.client/src/App.jsx
+++ PVP/pvp.client/src/App.jsx
@@ -2,7 +2,11 @@ import './App.css';
 import ResetPassword from './ResetPassword';
 function App() {
     return (
+
         <div className="App">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         <ResetPassword/>
         </div>
     )
