25a40a4f4a549f0db32725a796b21c32d1adbf78
Ailandas <ailandas.odminis@ktu.edu>
2024-03-10 00:41:59 +0200

    Buvo sukurti route'ingai ir tiek.

--- PVP/pvp.client/src/App.jsx
+++ PVP/pvp.client/src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css';
-import ResetPassword from './ResetPassword';
-function App() {
-    return (
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import HabitsPage from './pages/HabitsPage';
+import SmokingHabit from './pages/SmokingHabit';
+import NoPage from './pages/NoPage';
 
-        <div className="App">
-            <h1 className="font-italic underline">
-                Hello world!
-            </h1>
-        <ResetPassword/>
+export default function App() {
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<MainPage />} />
+                    <Route path="/mainpage" element={<MainPage />} />
+                    <Route path="/habitspage" element={<HabitsPage />} />
+                    <Route path="/smokinghabit" element={<SmokingHabit />} />
+                    <Route path="*" element={<NoPage />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
-
-export default App;
---
+++ PVP/pvp.client/src/pages/NoPage.jsx
@@ -0,0 +1,21 @@
+﻿import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+function Error404() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                textAlign: 'center', // Centruojame turinį horizontaliai
+            }}
+        >
+            <Typography variant="h1">Error 404</Typography>
+        </Box>
+    );
+}
+
+export default Error404;
