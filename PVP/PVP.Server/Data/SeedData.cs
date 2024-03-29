a32aa17e80c5dedcfb8f38fa6c3905d3957b8db6
Julius Kinderevičius <julkin@ktu.lt>
2024-03-29 03:44:26 +0200

    prideta checkinu seed data

--- PVP/PVP.Server/Data/SeedData.cs
+++ PVP/PVP.Server/Data/SeedData.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PVP.Server.Models;
+using System;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace PVP.Server.Data
 {
@@ -38,12 +41,57 @@ namespace PVP.Server.Data
                 Id = 3,
                 Name = "Water Habit"
             };
+            var checkIns = new List<CheckIn>() { };
+            DateTime currentDate  = DateTime.ParseExact("2024 03 02 17:20", "yyyy MM dd HH:mm", null).Date;
+            Random random = new Random();
+            for (int i = 0; i < 26;i++)
+            {
+                    // Incrementing the date by one day
+                    currentDate = currentDate.AddDays(1);
+                if (i % 3 == 0)
+                {
+                    // Creating a new check-in
+                    var newCheckIn = new CheckIn()
+                    {
+                        Id = i + 10, // Assuming the IDs continue from where we left off
+                        HabitUserId = 1,
+                        Mood = (Mood)random.Next(0, 4), // Random mood between 1 and 5
+                        Date = currentDate,
+                        Note = ""
+                    };
+                    checkIns.Add(newCheckIn);
+                }
+                if (i % 1 == 0)
+                {
+                    var newCheckIn1 = new CheckIn()
+                    {
+                        Id = i + 100, // Assuming the IDs continue from where we left off
+                        HabitUserId = 2,
+                        Mood = (Mood)random.Next(0, 4), // Random mood between 1 and 5
+                        Date = currentDate,
+                        Note = ""
+                    };
+                    checkIns.Add(newCheckIn1);
+                }
+                if (i % 2 == 0)
+                {
+                    var newCheckIn2 = new CheckIn()
+                    {
+                        Id = i + 1000, // Assuming the IDs continue from where we left off
+                        HabitUserId = 3,
+                        Mood = (Mood)random.Next(0, 4), // Random mood between 1 and 5
+                        Date = currentDate,
+                        Note = ""
 
+                    };
+                    checkIns.Add(newCheckIn2);
+                }
+            }
             var firstUserHabit = new HabitUser()
             {
                 Id = 1,
                 HabitId = 1,
-                UserId = 1
+                UserId = 1,
             };
             var secondUserHabit = new HabitUser()
             {
@@ -60,9 +108,11 @@ namespace PVP.Server.Data
             var users = new List<User>() { firstUser };
             var habits = new List<Habit>() { firstHabit, secondHabit, thirdHabit };
             var userhabits = new List<HabitUser> { firstUserHabit, secondUserHabit, thirdUserHabit };
+            
             await context.AddRangeAsync(users);
             await context.AddRangeAsync(habits);
             await context.AddRangeAsync(userhabits);
+            await context.AddRangeAsync(checkIns);
             await context.SaveChangesAsync();
         }
     }
--- PVP/PVP.Server/obj/Debug/net8.0/PVP.Server.AssemblyInfo.cs
+++ PVP/PVP.Server/obj/Debug/net8.0/PVP.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4b2c58d42c07bcae1be7a6e7a8aebaf2ec9728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6202d86a5bb4cdf976395f8ee9ad3cb7f8e39885")]
 [assembly: System.Reflection.AssemblyProductAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
