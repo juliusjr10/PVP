89d2afb4b3fa5fd0bd48266af102a0cee91657b1
tomakas54 <112956264+tomakas54@users.noreply.github.com>
2024-04-19 10:21:22 +0300

    gitignore gal pagaliau veiks

--- PVP/PVP.Server/obj/Debug/net8.0/PVP.Server.AssemblyInfo.cs
+++ PVP/PVP.Server/obj/Debug/net8.0/PVP.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddcab8921c5829a72ed90cd327d650bdac78a59e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0075c98d70fa64ad3d4795c6cfdb049621b1fbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
