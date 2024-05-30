109ebed229285a8c42dcf74600a2fa8b09e93bef
Guodx2 <guodastulpinaite@gmail.com>
2024-05-30 20:17:01 +0300

    public groups eilutės

--- PVP/PVP.Server/obj/Debug/net8.0/PVP.Server.AssemblyInfo.cs
+++ PVP/PVP.Server/obj/Debug/net8.0/PVP.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4c6513a68d1e45e12efb8a83a3b31cdd4cf5cac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d884d67e0a1ec56dd5c8d67eb5c3a580ad4f30a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PVP.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
